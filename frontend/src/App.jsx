6791ba29448dac9beb0185c4f2671b7d9bc23926
dinesh-321 <dineshkumaresan7@gmail.com>
2025-02-21 10:58:29 +0530

    assets folder created with Rdeg logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
+import "./App.css";
+
 
 function App() {
-  
   return (
     <>
-      <h1>Chamarket Project</h1>
-      <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-success">Success</button>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
