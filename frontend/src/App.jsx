f6403fdca040e040e38aa55bf5ace87f1726aae7
dinesh-321 <dineshkumaresan7@gmail.com>
2025-02-21 11:19:30 +0530

    Footer Check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Footer from "./components/common/Footer.jsx";
 
 
 function App() {
   return (
     <>
-
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,19 @@
+import './Footer.css'
+const Footer = () => {
+  return (
+    <Footer>
+        <div className="container footer p-2">
+            <div className="row">
+                <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
+                </div>  
+                <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
+                </div> 
+                <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
+                </div>           
+            </div>
+        </div>
+    </Footer>
+  )
+}
+
+export default Footer
